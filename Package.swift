565b85ad035913c01d9fd84ebe17b618cec79fd5
Rafael Cardoso <racasantos@icloud.com>
2025-01-22 13:01:39 -0500

    build fix

--- Package.swift
+++ Package.swift
@@ -1,17 +1,14 @@
-// swift-tools-version:5.10
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "mue",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v12)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
-        // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
-        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
     ],
     targets: [
@@ -22,22 +19,14 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-            ],
-            swiftSettings: swiftSettings
+            ]
         ),
         .testTarget(
             name: "AppTests",
             dependencies: [
                 .target(name: "App"),
                 .product(name: "XCTVapor", package: "vapor"),
-            ],
-            swiftSettings: swiftSettings
-        )
-    ],
-    swiftLanguageModes: [.v5]
+            ]
+        ),
+    ]
 )
-
-var swiftSettings: [SwiftSetting] { [
-    .enableUpcomingFeature("DisableOutwardActorInference"),
-    .enableExperimentalFeature("StrictConcurrency"),
-] }
