890b0e33a1f73ed5852c5fa0dd5e34b3a2fc3aee
Rafael Cardoso <racasantos@icloud.com>
2025-01-25 19:36:34 -0500

    fix

--- Package.swift
+++ Package.swift
@@ -9,12 +9,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
+                .product(name: "Fluent", package: "fluent"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
